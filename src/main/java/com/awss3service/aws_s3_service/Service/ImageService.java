93f1a2e80593c0576a466627932dfa25b37ed015
Arunjay22 <arunarunaj7@gmail.com>
2025-01-19 13:27:13 +0530

    code line added

--- src/main/java/com/awss3service/aws_s3_service/Service/ImageService.java
+++ src/main/java/com/awss3service/aws_s3_service/Service/ImageService.java
@@ -1,7 +1,14 @@
 package com.awss3service.aws_s3_service.Service;
 
+import com.awss3service.aws_s3_service.Repository.ImageRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImageService implements ImageServiceImpl {
+
+    private final ImageRepository imageRepository;
+
+    public ImageService(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
 }
