edd91251236c492251b1d09e92dd2052e4c4599b
Arunjay22 <arunarunaj7@gmail.com>
2025-01-19 21:52:13 +0530

    new lines added

--- src/main/java/com/awss3service/aws_s3_service/Controller/ImageController.java
+++ src/main/java/com/awss3service/aws_s3_service/Controller/ImageController.java
@@ -2,6 +2,7 @@ package com.awss3service.aws_s3_service.Controller;
 
 import com.awss3service.aws_s3_service.Service.ImageService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,7 @@ public class ImageController {
             return ResponseEntity.badRequest().body("Failed to upload file: " + e.getMessage());
         }
     }
+
 
 
 }
