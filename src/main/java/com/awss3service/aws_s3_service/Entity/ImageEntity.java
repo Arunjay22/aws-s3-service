14a4f9feac9892ee94877b6b3134a16d4fceba48
Arunjay22 <arunarunaj7@gmail.com>
2025-01-19 14:09:27 +0530

    code line added

--- src/main/java/com/awss3service/aws_s3_service/Entity/ImageEntity.java
+++ src/main/java/com/awss3service/aws_s3_service/Entity/ImageEntity.java
@@ -1,9 +1,6 @@
 package com.awss3service.aws_s3_service.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "Image_Table")
 public class ImageEntity {
 
     @Id
