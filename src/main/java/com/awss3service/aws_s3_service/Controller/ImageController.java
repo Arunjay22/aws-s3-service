24a40b99e8cb7b1892700367ea1f917d7344d2ec
Arunjay22 <arunarunaj7@gmail.com>
2025-01-19 16:47:53 +0530

    new lines added

--- src/main/java/com/awss3service/aws_s3_service/Controller/ImageController.java
+++ src/main/java/com/awss3service/aws_s3_service/Controller/ImageController.java
@@ -1,7 +1,11 @@
 package com.awss3service.aws_s3_service.Controller;
 
 import com.awss3service.aws_s3_service.Service.ImageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ImageController {
@@ -12,5 +16,13 @@ public class ImageController {
         this.imageService = imageService;
     }
 
-
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            String url = imageService.uploadImage(file);
+            return ResponseEntity.ok("File uploaded successfully: " + url);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Failed to upload file: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/awss3service/aws_s3_service/Service/ImageService.java
+++ src/main/java/com/awss3service/aws_s3_service/Service/ImageService.java
@@ -1,14 +1,51 @@
 package com.awss3service.aws_s3_service.Service;
 
+import com.awss3service.aws_s3_service.Entity.ImageEntity;
 import com.awss3service.aws_s3_service.Repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
 
 @Service
 public class ImageService implements ImageServiceImpl {
 
     private final ImageRepository imageRepository;
 
+    private S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
     public ImageService(ImageRepository imageRepository) {
         this.imageRepository = imageRepository;
+    }
+
+    public String uploadImage(MultipartFile file) throws Exception {
+
+        String originalFilename = file.getOriginalFilename();
+
+        try {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(originalFilename)
+                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+
+            String url = String.format("http://%s.s3.amazonaws.com/%s", bucketName, originalFilename);
+            ImageEntity imageEntity = new ImageEntity();
+            imageEntity.setImageName(originalFilename);
+            imageEntity.setImageUrl(url);
+            return url;
+        } catch (Exception e) {
+            throw new IOException("Something Went wrong");
+
+        }
+
     }
 }
